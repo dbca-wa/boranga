324e60b874bc5cecaeefc09034d9724d0561a498
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-06-06 14:17:00 +0800

    Fix readonly username

--- reporting_database_rebuild.sh
+++ reporting_database_rebuild.sh
@@ -44,8 +44,8 @@ done
 # GRANT SELECT to boranga_ro account
 for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename not like 'pg\_%' and tablename not like 'sql\_%';" -t);
   do
-  echo "GRANT SELECT ON $I TO boranga_ro;";
-  psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "GRANT SELECT ON $I TO boranga_ro; " -t
+  echo "GRANT SELECT ON $I TO boranga_reporting_uat_ro;";
+  psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "GRANT SELECT ON $I TO boranga_reporting_uat_ro; " -t
 done
 
 rm /dbdumps/ledger_core_prod.sql
