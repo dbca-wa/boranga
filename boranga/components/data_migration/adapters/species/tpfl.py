05a378864be411a99826944e1a9a4319220a0920
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-09-10 11:43:51 +0800

    Only populate conservation_plan_reference if there is at least one of the dependent values present.

--- boranga/components/data_migration/adapters/species/tpfl.py
+++ boranga/components/data_migration/adapters/species/tpfl.py
@@ -20,7 +20,8 @@ class SpeciesTpflAdapter(SourceAdapter):
             canonical = schema.map_raw_row(raw)
             RP_COMMENTS = canonical.get("RP_COMMENTS", "").strip()
             RP_EXP_DATE = canonical.get("RP_EXP_DATE", "").strip()
-            canonical["conservation_plan_reference"] = f"{RP_COMMENTS}{RP_EXP_DATE}"
+            if RP_COMMENTS or RP_EXP_DATE:
+                canonical["conservation_plan_reference"] = f"{RP_COMMENTS}{RP_EXP_DATE}"
             canonical["group_type_id"] = GroupType.objects.get(name="flora").id
             rows.append(canonical)
         return ExtractionResult(rows=rows, warnings=warnings)
