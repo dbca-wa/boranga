f1149aacc10a3ea12e4719d5128d949ff3514f6f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-09-16 11:58:56 +0800

    Move the unqiue constraint from scientific_name to taxon_name_id to suit source data.

--- boranga/components/species_and_communities/models.py
+++ boranga/components/species_and_communities/models.py
@@ -218,10 +218,8 @@ class Taxonomy(BaseModel):
     - Table
     """
 
-    taxon_name_id = models.IntegerField(null=True, blank=True)
-    scientific_name = models.CharField(
-        max_length=512, null=True, blank=True, unique=True
-    )
+    taxon_name_id = models.IntegerField(null=True, blank=True, unique=True)
+    scientific_name = models.CharField(max_length=512, null=True, blank=True)
     kingdom_id = models.CharField(max_length=100, null=True, blank=True)
     kingdom_fk = models.ForeignKey(
         Kingdom, on_delete=models.SET_NULL, null=True, blank=True, related_name="taxons"
