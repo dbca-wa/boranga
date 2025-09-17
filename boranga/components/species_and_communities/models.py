302a640c4f0cd9c9a59c7b1a5530676d374aa62e
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-09-17 16:28:17 +0800

    Add datetime_created and datetime_updated fields to Taxonomy model to better assist with future investigations.

--- boranga/components/species_and_communities/models.py
+++ boranga/components/species_and_communities/models.py
@@ -246,6 +246,9 @@ class Taxonomy(BaseModel):
     genera_id = models.IntegerField(null=True, blank=True)
     genera_name = models.CharField(max_length=512, null=True, blank=True)
 
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
     class Meta:
         app_label = "boranga"
         ordering = ["scientific_name"]
