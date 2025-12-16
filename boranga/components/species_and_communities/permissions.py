2b2ef6dbb4ab7e92d8393f8641ce7d9aed5789bb
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-12-16 11:25:45 +0800

    Revert "Enhance SpeciesCommunitiesPermission to check conservation status referrals for object permissions"
    This reverts commit 9b7b9113a19927c6bea1f1f4cbe341e514550ca8.

--- boranga/components/species_and_communities/permissions.py
+++ boranga/components/species_and_communities/permissions.py
@@ -6,8 +6,6 @@ from rest_framework.permissions import BasePermission
 from boranga.helpers import (
     is_conservation_status_approver,
     is_conservation_status_assessor,
-    is_conservation_status_referee,
-    is_internal,
     is_occurrence_approver,
     is_occurrence_assessor,
     is_readonly_user,
@@ -33,26 +31,7 @@ class SpeciesCommunitiesPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            if is_internal(request):
-                return True
-            if is_conservation_status_referee(request):
-                # Check if the user is a referee for a conservation status related to this species/community
-                from boranga.components.conservation_status.models import (
-                    ConservationStatusReferral,
-                )
-
-                # Check if the object is a Species or Community
-                if hasattr(obj, "conservation_status"):
-                    # This covers Species and Community models as they have reverse relation 'conservation_status'
-                    # So obj.conservation_status.all() gives all CS for this species.
-
-                    # We need to check if ANY of these CS has a referral for this user.
-                    return ConservationStatusReferral.objects.filter(
-                        referral=request.user.id,
-                        conservation_status__in=obj.conservation_status.all(),
-                    ).exists()
-
-            return False
+            return True
 
         return is_species_communities_approver(request)
 
