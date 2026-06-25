22674e09556fe6dcb4b18eb638e9be6f6357a34c
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2026-06-25 11:36:12 +0800

    Allow approver to edit cc email

--- boranga/frontend/boranga/src/components/internal/occurrence/ocr_decline.vue
+++ boranga/frontend/boranga/src/components/internal/occurrence/ocr_decline.vue
@@ -38,7 +38,6 @@
                                 <input
                                     v-model="decline.cc_email"
                                     type="text"
-                                    disabled
                                     style="width: 70%"
                                     class="form-control"
                                     name="cc_email"
